a089c512013a276a77b5f5cd5ec65e19746983e6
LeahESc <leahemily1@gmail.com>
2020-12-07 14:12:33 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainId = document.getElementbyId('main');
+    mainId.remove();
+
+const newHeader = document.createElement("h1");
+    document.body.appendChild(newHeader).createElement("id = 'victory'");
+    newHeader.innerHTML ='Leah is the champion';
