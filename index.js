b195362f97146b655072f8ce23d93990203633d8
jwatts82 <jessperr182@hotmail.com>
2020-12-08 20:42:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainId = document.getElementById('main');
+    mainId.remove();
+
+const newHeader = document.createElement('h1')
+    newHeader.id = 'victory'; 
+    newHeader.innerHTML = 'Jessica is the champion';
