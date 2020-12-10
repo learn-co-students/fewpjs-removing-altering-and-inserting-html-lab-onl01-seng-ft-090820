dcbb9847afba5beec9a486b84d4427112c0cd1e7
csdj92 <csdj92@gmail.com>
2020-12-10 09:22:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainId = document.getElementById('main');
+    mainId.remove();
+
+const newHeader = document.createElement("h1");
+    newHeader.id = 'victory';
+    newHeader.innerHTML ='Cj is the champion'; 
